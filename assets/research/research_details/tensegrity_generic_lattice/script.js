ddf1f464e99844ab69128cb200666efad9c7b771
Vishal Sudhakar <32366332+vishalsudhakar@users.noreply.github.com>
2023-10-22 13:09:29 -0400

    Add files via upload

---
+++ assets/research/research_details/tensegrity_generic_lattice/script.js
@@ -0,0 +1,14 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuIcon = document.querySelector('.mobile-menu-icon');
+    const topMenu = document.querySelector('.top-menu');
+
+    mobileMenuIcon.addEventListener('click', () => {
+        topMenu.classList.toggle('active');
+        mobileMenuIcon.classList.toggle('active');
+        
+    });
+});
+
+
+
